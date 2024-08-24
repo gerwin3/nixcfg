3ebd42d487b0dd4c1873a2cf5b361cb215c95116
Gerwin van der Lugt <account+git@gerwin3.com>
2024-08-24 13:07:07 +0200

    fix systemd boot logging rendering over tuigreet

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,5 +1,10 @@
 { ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
+  boot = {
+    loader.systemd-boot.enable = true;
+
+    # Force boot systemd output on tty1 so we free up tty2 for greetd.
+    kernelParams = [ "console=tty1" ];
+  };
 }
--- nixos/modules/greetd.nix
+++ nixos/modules/greetd.nix
@@ -5,9 +5,12 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway --unsupported-gpu";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu'";
         user = "gerwin";
       };
     };
+
+    # Use tty2 since tty1 is used by systemd boot logging.
+    vt = 2;
   };
 }
