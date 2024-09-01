c330857f5d7382cc4438511d3ceb4181c987b3d9
Gerwin van der Lugt <account+git@gerwin3.com>
2024-09-01 17:14:58 +0200

    added brightness keybinds

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -2,4 +2,12 @@
 
 {
   programs.light.enable = true;
+
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+    ];
+  };
 }
