8c2a062b5a8b4e45e74b6e3f17c358026315bf0b
Gerwin van der Lugt <account+git@gerwin3.com>
2024-09-01 17:28:42 +0200

    brightness keys were swapped

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -6,8 +6,8 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
     ];
   };
 }
