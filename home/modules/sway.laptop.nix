1584fd4e25108d52a68cff75bf4d11f7457904bf
Gerwin van der Lugt <account+git@gerwin3.com>
2024-09-01 21:14:31 +0200

    natural scrolling on touchpad

--- home/modules/sway.laptop.nix
+++ home/modules/sway.laptop.nix
@@ -3,6 +3,12 @@
 {
   wayland.windowManager.sway = {
     config = {
+      input = {
+        "2362:628:PIXA3854:00_093A:0274_Touchpad" = {
+          natural_scroll = "enabled";
+          tap = "enable";
+        };
+      };
       output = {
         "eDP-1".bg = "${../resources/background-horizontal.png} fill";
       };
