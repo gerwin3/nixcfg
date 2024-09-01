14fc5c1020674e3d9296eb64707e829f57ebd81a
Gerwin van der Lugt <account+git@gerwin3.com>
2024-09-01 17:11:28 +0200

    brightness

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -2,13 +2,4 @@
 
 {
   programs.light.enable = true;
-
-  # TODO: Might need this?
-  # services.actkbd = {
-  #   enable = true;
-  #   bindings = [
-  #     { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-  #     { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
-  #   ];
-  # };
 }
