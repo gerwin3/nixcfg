a740df3dafb9c92428032e5df455f3f2d8213f2c
Gerwin van der Lugt <account+git@gerwin3.com>
2024-09-25 10:46:23 +0200

    store sway logs

--- nixos/modules/greetd.nix
+++ nixos/modules/greetd.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu'";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu |& tee /tmp/sway.log'";
         user = "gerwin";
       };
     };
