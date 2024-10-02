a1015a19bc9aa74872b0a4bbf9b4d74f5d9a524a
Gerwin van der Lugt <account+git@gerwin3.com>
2024-10-02 11:19:22 +0200

    fix init.lua relative file path

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -4,7 +4,7 @@
   programs.neovim = {
     defaultEditor = true;
     enable = true;
-    extraLuaConfig = lib.fileContents ../../extra/neovim/init.lua;
+    extraLuaConfig = lib.fileContents ../extra/neovim/init.lua;
     # For most projects it is recommended to have the development dependencies be
     # part of the devshell. Configuration file formats are an exception to this
     # since they can appear anywhere. So we load LSP support for them here:
