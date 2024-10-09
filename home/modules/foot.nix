2b500ddd31d644f1325afb339403bbc14cf91011
Gerwin van der Lugt <account+git@gerwin3.com>
2024-10-09 14:13:29 +0200

    fix copy-paste

--- home/modules/foot.nix
+++ home/modules/foot.nix
@@ -7,7 +7,10 @@
       main = {
         font = if variant == "laptop" then "Iosevka Nerd Font:size=11" else "Iosevka Nerd Font:size=10";
         pad = "8x8";
-        selection-target = "clipboard";
+        # Had this enable previously because it causes foot to copy stuff into
+        # the clipboard correctly. Unfortunately, it also breaks copying stuff
+        # into foot for some unknown reason. So don't enable it!
+        # selection-target = "both";
         term = "xterm-256color";
       };
     };
