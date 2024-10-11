d5e951ae93ada0dc0538df2dd171edf677e6f371
Gerwin van der Lugt <account+git@gerwin3.com>
2024-10-11 13:09:13 +0200

    fixed sway overlay

--- flake.nix
+++ flake.nix
@@ -30,15 +30,37 @@
           {
             nixpkgs.overlays = [
               inputs.nur.overlay
-              # FIXME: Overlay sway so we get the latest master version which
-              # fixes the tearing issue.
-              (self: super: {
-                sway = super.sway.overrideAttrs (old: {
-                  src = builtins.fetchGit {
-                    url = "https://github.com/swaywm/sway.git";
-                    rev = "7f1cd0b73ba3290f8ee5f81fdf7f1ffa4c642ea7";
+              # FIXME: Temporarily overlay sway and wlroots so we get the latest
+              # master version which fixes the tearing issue, until Sway 1.10 is
+              # released. Took this excellent overlay from here:
+              # https://github.com/lovesegfault/nix-config/blob/c6bff2fde78b7b68d143bb78886e73b68b6eb0c0/nix/overlays/sway-unstable.nix
+              (final: prev: {
+                wlroots-unstable = (final.wlroots_0_17.overrideAttrs (old: {
+                  version = "unstable-2024-08-13";
+                  src = final.fetchFromGitLab {
+                    domain = "gitlab.freedesktop.org";
+                    owner = "wlroots";
+                    repo = "wlroots";
+                    rev = "2c64f36e8886d1f26daeb2a4ee79f3f9dd3d4c85";
+                    hash = "sha256-elHu3d82STGB+pTGPj1K9eOyWGsotqUX4e0ZQ+db4Rg=";
                   };
-                });
+                  buildInputs = (old.buildInputs or [ ]) ++ [
+                    final.lcms2
+                  ];
+                })).override { };
+                sway-unwrapped = (prev.sway-unwrapped.overrideAttrs (old: {
+                  version = "unstable-2024-08-11";
+                  src = final.fetchFromGitHub {
+                    owner = "swaywm";
+                    repo = "sway";
+                    rev = "b44015578a3d53cdd9436850202d4405696c1f52";
+                    hash = "sha256-gTsZWtvyEMMgR4vj7Ef+nb+wcXkwGivGfnhnBIfPHOA=";
+                  };
+                  nativeBuildInputs = with final; (old.nativeBuildInputs or [ ]) ++ [ bash-completion fish ];
+                  mesonFlags = builtins.filter
+                    (f: f != "-Dxwayland=enabled")
+                    (old.mesonFlags or [ ]);
+                })).override { wlroots = final.wlroots-unstable; };
               })
             ];
           }
