fe48b70cd83ba4fd6990362dbff56131a988bc74
Gerwin van der Lugt <account+git@gerwin3.com>
2024-10-22 11:01:24 +0200

    removed move and take mode

--- home/modules/sway.desktop.nix
+++ home/modules/sway.desktop.nix
@@ -21,42 +21,6 @@
           bg = "${../resources/background-horizontal.png} fill";
         };
       };
-      modes = {
-        # Move mode allows switching to any workspace without having to reach
-        # for the number keys. Activate move mode by pressing Mod+m, then the
-        # mappings are:
-        # * e, r -> 1, 2 (workspaces on monitor 1)
-        # * a, s, d, f -> 3, 4, 5, 6 (workspaces on monitor 2)
-        # * j, m, k, ; -> 7, 8, 9, 10 (workspaces on monitor 3)
-        move = {
-          "e" = "workspace 1; mode default";
-          "r" = "workspace 2; mode default";
-          "a" = "workspace 3; mode default";
-          "s" = "workspace 4; mode default";
-          "d" = "workspace 5; mode default";
-          "f" = "workspace 6; mode default";
-          "j" = "workspace 7; mode default";
-          "k" = "workspace 8; mode default";
-          "l" = "workspace 9; mode default";
-          "Semicolon" = "workspace 10; mode default";
-          "Escape" = "mode default";
-        };
-        # Take mode is the same as move mode but it takes the currently
-        # selected window with it.
-        take = {
-          "e" = "move container to workspace 1; workspace 1; mode default";
-          "r" = "move container to workspace 2; workspace 2; mode default";
-          "a" = "move container to workspace 3; workspace 3; mode default";
-          "s" = "move container to workspace 4; workspace 4; mode default";
-          "d" = "move container to workspace 5; workspace 5; mode default";
-          "f" = "move container to workspace 6; workspace 6; mode default";
-          "j" = "move container to workspace 7; workspace 7; mode default";
-          "k" = "move container to workspace 8; workspace 8; mode default";
-          "l" = "move container to workspace 9; workspace 9; mode default";
-          "Semicolon" = "move container to workspace 10; workspace 10; mode default";
-          "Escape" = "mode default";
-        };
-      };
       seat = {
         "*" = {
           xcursor_theme = "Adwaita 24";
