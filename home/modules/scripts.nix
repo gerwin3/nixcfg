184b6eca17ae2399fbe4399c659bf4391b318892
Gerwin van der Lugt <account+git@gerwin3.com>
2024-10-22 11:01:13 +0200

    added script for setting up right monitor

--- home/modules/scripts.nix
+++ home/modules/scripts.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = [
+    (pkgs.writeShellScriptBin "right" (builtins.readFile ../scripts/right.sh))
     (pkgs.writeShellScriptBin "preheat" (builtins.readFile ../scripts/preheat.sh))
     (pkgs.writeShellScriptBin "worktree" (builtins.readFile ../scripts/worktree.sh))
   ];
