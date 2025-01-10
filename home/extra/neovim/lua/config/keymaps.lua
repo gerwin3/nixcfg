b7c007165e2ecb414812f95cd73515bac8c6832e
Gerwin van der Lugt <account+git@gerwin3.com>
2025-01-10 13:11:03 +0100

    layzvim: remove diagnostic keymaps that were already there

--- home/extra/neovim/lua/config/keymaps.lua
+++ home/extra/neovim/lua/config/keymaps.lua
@@ -1,4 +1,1 @@
-vim.keymap.set("n", "]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, {})
-vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, {})
-vim.keymap.set("n", "]w", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARNING }) end, {})
-vim.keymap.set("n", "[w", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARNING }) end, {})
+
