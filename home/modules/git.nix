5bbbf01a46a403647c93d4874cf9c52e7f4e62a6
Gerwin van der Lugt <account+git@gerwin3.com>
2025-02-25 11:19:25 +0100

    git: some extra options (inspired by https://blog.gitbutler.com/how-git-core-devs-configure-git/)

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -6,14 +6,19 @@
     userName = "Gerwin van der Lugt";
     userEmail = "account+git@gerwin3.com";
     extraConfig = {
+      branch.sort = "-committerdate";
       commit.verbose = true;
       core.editor = "vim";
       diff.algorithm = "histogram";
+      diff.colorMoved = "plain";
+      diff.renames = true;
       fetch.prune = true;
+      fetch.pruneTags = true;
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = false;
       rerere.enabled = true;
+      tag.sort = "version:refname";
     };
   };
 }
