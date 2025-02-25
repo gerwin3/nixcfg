32cd88928ff47505689559864fd15184645ee055
Gerwin van der Lugt <account+git@gerwin3.com>
2025-02-25 14:29:34 +0100

    lazyvim: zls: finetuning config

--- home/extra/neovim/lua/plugins/nvim-lspconfig.lua
+++ home/extra/neovim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        zls = {
+          -- see https://github.com/zigtools/zls/blob/master/src/Config.zig
+          enable_argument_placeholders = false,
+          enable_build_on_save = true,
+          force_autofix = true,
+          warn_style = true,
+        },
+      }
     },
   },
 }
