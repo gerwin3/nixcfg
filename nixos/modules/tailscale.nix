21c7146c0aef4c6b6c4997ffa2ccb449cd6560e5
Gerwin van der Lugt <account+git@gerwin3.com>
2025-03-26 16:30:10 +0100

    tailscale: add autoconnect

--- nixos/modules/tailscale.nix
+++ nixos/modules/tailscale.nix
@@ -1,3 +1,30 @@
+{ pkgs, ... }:
+
 {
   services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    after = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = with pkgs; ''
+      sleep 2
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then
+        exit 0
+      fi
+      ${tailscale}/bin/tailscale up -authkey tskey-examplekeyhere
+    '';
+  };
 }
