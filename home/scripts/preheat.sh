578a42b6278fb1206158adb1c409af60b4a368d2
Gerwin van der Lugt <account+git@gerwin3.com>
2025-04-14 11:30:48 +0200

    preheat.sh: formatting

--- home/scripts/preheat.sh
+++ home/scripts/preheat.sh
@@ -4,26 +4,26 @@
 # so that all further invocations of `nix develop` are faster since the
 # derivations and what not will already be built :). This script runs on startup
 # so when I'm done with mail and other boring stuff all code should be preheated
-# and ready to!
+# and ready to go!
 
 set -e
 set -o pipefail
 set -x
 
-preheat_dir () {
-    dir=$1
-    if [ -f "${dir}flake.nix" ] || [ -f "${dir}default.nix" ]; then
-        echo "preheating $dir"
-        nix develop "$dir" --command true || true
-    fi
+preheat_dir() {
+  dir=$1
+  if [ -f "${dir}flake.nix" ] || [ -f "${dir}default.nix" ]; then
+    echo "preheating $dir"
+    nix develop "$dir" --command true || true
+  fi
 }
 
-preheat_subdirs () {
-    for dir in "$1"/*/; do
-        if [ -d "$dir" ]; then
-            preheat_dir "$dir"
-        fi
-    done
+preheat_subdirs() {
+  for dir in "$1"/*/; do
+    if [ -d "$dir" ]; then
+      preheat_dir "$dir"
+    fi
+  done
 }
 
 preheat_subdirs ~/code
