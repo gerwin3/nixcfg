4bb8f687933ff4bd6a9f47355b9650ae33b56ada
Gerwin van der Lugt <account+git@gerwin3.com>
2025-04-15 13:56:59 +0200

    apply.sh: first clean out /etc/nixos/{nixos,home} before copying

--- apply.sh
+++ apply.sh
@@ -11,12 +11,16 @@ if [[ "$1" == "--update" ]]; then
 fi
 
 sudo cp -r flake.nix /etc/nixos
+
+sudo rm -rf /etc/nixos/nixos/*
+sudo rm -rf /etc/nixos/home/*
+
 sudo cp -r nixos /etc/nixos
 sudo cp -r home /etc/nixos
 
 if [[ -d "../nixcfg-priv" ]]; then
-    sudo cp -r ../nixcfg-priv/nixos /etc/nixos
-    sudo cp -r ../nixcfg-priv/home /etc/nixos
+  sudo cp -r ../nixcfg-priv/nixos /etc/nixos
+  sudo cp -r ../nixcfg-priv/home /etc/nixos
 fi
 
 if [[ "$update" = true ]]; then
