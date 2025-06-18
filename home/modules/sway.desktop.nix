04a666efc18e66de30d4a5d3b83e042481b01a27
Gerwin van der Lugt <account+git@gerwin3.com>
2025-06-18 13:03:04 +0200

    fix monitor names

--- home/modules/sway.desktop.nix
+++ home/modules/sway.desktop.nix
@@ -4,18 +4,18 @@
   wayland.windowManager.sway = {
     config = {
       output = {
-        "DP-4" = {
+        "DP-1" = {
           pos = "0 420";
           scale = "2";
           bg = "${../resources/background-horizontal.png} fill";
         };
-        "DP-5" = {
+        "DP-2" = {
           pos = "1920 0";
           scale = "2";
           transform = "270";
           bg = "${../resources/background-vertical.png} fill";
         };
-        "DP-6" = {
+        "DP-3" = {
           pos = "3000 440";
           scale = "2";
           bg = "${../resources/background-horizontal.png} fill";
@@ -29,43 +29,43 @@
       workspaceOutputAssign = [
         {
           workspace = "1";
-          output = "DP-4";
+          output = "DP-1";
         }
         {
           workspace = "2";
-          output = "DP-4";
+          output = "DP-1";
         }
         {
           workspace = "3";
-          output = "DP-5";
+          output = "DP-2";
         }
         {
           workspace = "4";
-          output = "DP-5";
+          output = "DP-2";
         }
         {
           workspace = "5";
-          output = "DP-5";
+          output = "DP-2";
         }
         {
           workspace = "6";
-          output = "DP-5";
+          output = "DP-2";
         }
         {
           workspace = "7";
-          output = "DP-6";
+          output = "DP-3";
         }
         {
           workspace = "8";
-          output = "DP-6";
+          output = "DP-3";
         }
         {
           workspace = "9";
-          output = "DP-6";
+          output = "DP-3";
         }
         {
           workspace = "10";
-          output = "DP-6";
+          output = "DP-3";
         }
       ];
     };
