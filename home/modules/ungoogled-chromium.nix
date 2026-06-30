9c709f346f7fc909ec1cea1494a7a0668b1d226e
Gerwin van der Lugt <account+git@gerwin3.com>
2026-06-30 14:56:33 +0200

    ungoogled-chromium: vendor extensions

--- home/modules/ungoogled-chromium.nix
+++ home/modules/ungoogled-chromium.nix
@@ -13,50 +13,42 @@
     package = pkgs.ungoogled-chromium;
     extensions =
       let
-        createChromiumExtensionFor =
-          browserVersion:
+        createChromiumExtension =
           {
             id,
-            sha256,
             version,
+            crxFile,
           }:
           {
             inherit id;
-            crxPath = builtins.fetchurl {
-              url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
-              name = "${id}.crx";
-              inherit sha256;
-            };
+            crxPath = ../extra/ungoogled-chromium/plugins + "/${crxFile}";
             inherit version;
           };
-        createChromiumExtension = createChromiumExtensionFor (
-          lib.versions.major pkgs.ungoogled-chromium.version
-        );
       in
       [
         # 1Password
         (createChromiumExtension {
           id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa";
-          sha256 = "sha256:1ry8h3as008b9d9jr3q9jaqxxfnvylalp3jcsyd21ap8khzs1p2p";
           version = "8.12.2.37";
+          crxFile = "1password.crx";
         })
         # uBlock Origin Lite
         (createChromiumExtension {
           id = "ddkjiahejlhfcafbddmgiahcphecmpfh";
-          sha256 = "sha256:1rlyjrky8yxij7zsmh57lwhlz2wizgdzp78yp5qmryxwydpmcy23";
           version = "2026.215.1801";
+          crxFile = "ublock-origin-lite.crx";
         })
         # Vimium
         (createChromiumExtension {
           id = "dbepggeogbaibhgnhhndojpepiihcmeb";
-          sha256 = "sha256:0f0gm9p88y8d26lv0cy3i2idl8whn7bphwm2p3cvrl5i5nh6waq1";
           version = "2.4.0";
+          crxFile = "vimium.crx";
         })
         # Catppuccin Theme
         (createChromiumExtension {
           id = "bkkmolkhemgaeaeggcmfbghljjjoofoh";
-          sha256 = "sha256:01bzcbpxgrzmwp514q7fgp8g144bs7rzfmp0r07g6ysq0ykbhz3v";
           version = "5.0.0";
+          crxFile = "catppuccin-theme.crx";
         })
 
       ];
