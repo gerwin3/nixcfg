f479f1a0e0e1d8fa2ee23a45b1d62d88e85bbbda
Gerwin van der Lugt <account+git@gerwin3.com>
2026-09-03 16:41:45 +0200

    neovim: some more useful options in default configuration

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.neovim = {
     defaultEditor = true;
     enable = true;
-    extraPackages = with pkgs; [ ];
+    extraPackages = [ ];
+    initLua = ''
+      vim.opt.number = true
+      vim.opt.cursorline = true
+      vim.opt.cursorlineopt = "number"
+      vim.opt.scrolloff = 10
+      vim.opt.splitright = true
+      vim.opt.tabstop = 4
+      vim.opt.shiftwidth = 4
+      vim.opt.softtabstop = 4
+      vim.opt.expandtab = true
+      vim.opt.smarttab = true
+      vim.opt.clipboard:append('unnamedplus')
+    '';
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
